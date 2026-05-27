a6746a8a078577a09ceddf2a635ec895f436b9a1
10jul <10jul@Julio-710C>
2026-05-26 23:38:23 -0600

    Se creo la estructura del Proyecto.

---
+++ SistemaHospital/src/main/java/com/hospital/main/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.hospital.main;
+
+/**
+ *
+ * @author 10jul
+ */
+public class Main {
+    
+}
