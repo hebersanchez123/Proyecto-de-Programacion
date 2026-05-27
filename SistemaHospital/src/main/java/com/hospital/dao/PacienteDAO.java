1cad51caa2ddca771807301d4ca013f675a9cd28
Usuario <Usuario@DESKTOP-11DB57S>
2026-05-27 00:01:04 -0600

    Ya está el paquete que ayuda a reflejar todo en la base de datos

--- SistemaHospital/src/main/java/com/hospital/dao/PacienteDAO.java
+++ SistemaHospital/src/main/java/com/hospital/dao/PacienteDAO.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.hospital.dao;
 
-/**
- *
- * @author 10jul
- */
+import com.hospital.database.ConexionDB;
+import com.hospital.model.Paciente;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PacienteDAO {
-    
+
+    public void guardarPaciente(Paciente paciente) {
+
+        String sql = """
+                INSERT INTO pacientes
+                (nombre, edad, dpi, sintomas, prioridad)
+                VALUES (?, ?, ?, ?, ?)
+                               """;
+
+        try (Connection conn = ConexionDB.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, paciente.getNombre());
+            stmt.setInt(2, paciente.getEdad());
+            stmt.setString(3, paciente.getDpi());
+            stmt.setString(4, paciente.getSintomas());
+            stmt.setString(5, paciente.getPrioridad().name());
+
+            stmt.executeUpdate();
+
+            System.out.println("Paciente guardado");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
