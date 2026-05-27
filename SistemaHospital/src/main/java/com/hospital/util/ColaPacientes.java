c3c31eb2e8be087ce97d5379100d4cb944fa7819
Usuario <Usuario@DESKTOP-11DB57S>
2026-05-27 00:15:05 -0600

    Ya está el paquete y clase que ayuda a priorizar a los pacientes con más urgencia

--- SistemaHospital/src/main/java/com/hospital/util/ColaPacientes.java
+++ SistemaHospital/src/main/java/com/hospital/util/ColaPacientes.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.hospital.util;
 
-/**
- *
- * @author 10jul
- */
+import com.hospital.model.Paciente;
+import com.hospital.model.PacienteComparator;
+
+import java.util.PriorityQueue;
+
 public class ColaPacientes {
-    
+
+    private PriorityQueue<Paciente> cola;
+
+    public ColaPacientes() {
+        cola = new PriorityQueue<>(new PacienteComparator());
+    }
+
+    public void agregarPaciente(Paciente paciente) {
+        cola.add(paciente);
+    }
+
+    public Paciente atenderPaciente() {
+        return cola.poll();
+    }
+
+    public PriorityQueue<Paciente> getCola() {
+        return cola;
+    }
 }
