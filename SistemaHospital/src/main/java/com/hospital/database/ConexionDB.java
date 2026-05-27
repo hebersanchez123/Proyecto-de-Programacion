c5876fefc373b38b36096e077049c597f579668c
Usuario <Usuario@DESKTOP-11DB57S>
2026-05-27 00:12:29 -0600

    Ya está el paquete que ayuda a conectar el código de apache con la base de datos de MySQL

--- SistemaHospital/src/main/java/com/hospital/database/ConexionDB.java
+++ SistemaHospital/src/main/java/com/hospital/database/ConexionDB.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.hospital.database;
 
-/**
- *
- * @author 10jul
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionDB {
-    
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/hospital_prioridad";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection conectar() {
+
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (SQLException e) {
+
+            System.out.println("Error de conexión");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
